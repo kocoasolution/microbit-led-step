3592caeca617d224cb713b77615fb135eca3d151
kocoasolution <wjy0402@kocoa.or.kr>
2024-10-10 11:10:46 +0900

    main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 namespace screenMagic {
+    /**
+     * 가로로 불켜지게 하기
+     */
+    //% blockId=screenmagicplotat
+    //% block="plot at $index"
+    //% index.min=0 index.max=25
     export function plotAt(index: number): void {
         index |= 0;
         const y = Math.floor(index / 5);
